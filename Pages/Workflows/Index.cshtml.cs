baa20ee48c40d3117a47e7cbcb0c1da9bda18268
boeckwi <w.boeck@walz.de>
2025-03-06 14:15:36 +0100

    read workflows via api instead of EF

--- Pages/Workflows/Index.cshtml.cs
+++ Pages/Workflows/Index.cshtml.cs
@@ -2,23 +2,29 @@
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
 using WorkerDemo.Generic.WorkflowEF;
+using WorkflowCore.Interface;
+using WorkflowCore.Models;
 
 namespace WorkerDemo.Pages.Workflows
 {
     public class IndexModel : PageModel
     {
-        private readonly WorkflowContext db;
+        readonly IWorkflowHost wf;
+        readonly WorkflowContext db;
 
-        public IndexModel(WorkflowContext db)
+        public IndexModel(IWorkflowHost wf, WorkflowContext db)
         {
+            this.wf = wf;
             this.db = db;
         }
 
-        public IList<Workflow> Workflow { get;set; } = default!;
+        public IList<WorkflowInstance> Workflows { get;set; } = default!;
 
         public async Task OnGetAsync()
         {
-            Workflow = await db.Workflows.ToListAsync();
+            Workflows = (await wf.PersistenceStore.GetWorkflowInstances(
+                status: null, type: null, createdFrom: null, createdTo: null,
+                skip: 0, take: 100)).ToList();
         }
 
         public async Task<IActionResult> OnPostCleanup(string instanceId)
